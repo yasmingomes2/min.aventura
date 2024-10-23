136abb4279e53e8c81ffc61567ddd18a7f3e2f61
yasmingomes2 <00001114172303SP@al.educacao.sp.gov.br>
2024-10-22 21:28:17 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const avanca = document.querySelectorAll(" .bt-proximo")
+avanca.forEach(button=> {
+    button.addEvenListener("click ,function")
+     const atual=document.querySelector(".arivo");
+     const oproximoPasso = "passo-" + this . getAttribute("data-proximo");
+     atual.classList.remove("ativo");
+     document.getElementById(proximoPasso).classList.add("ativo");  
+
+    })
+
